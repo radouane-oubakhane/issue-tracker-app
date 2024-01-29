eaba3dc7ac0d8133d7ef1af01f03462c30baaca3
radouane <oubakhaneradouane@gmail.com>
2024-01-30 00:19:26 +0100

    Show a confirmation dialog box

--- app/issues/[id]/edit/DeleteIssueButton.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from '@radix-ui/themes'
-
-const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
-  return (
-    <Button color='red'>Delete Issue</Button>
-  )
-}
-
-export default DeleteIssueButton
