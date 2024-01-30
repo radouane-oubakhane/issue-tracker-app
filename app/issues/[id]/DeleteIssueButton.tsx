405bd99d79b4347bd0915019d34dad1e93885dcd
radouane <oubakhaneradouane@gmail.com>
2024-01-30 22:47:28 +0100

    Remove duplicate skeletons

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -3,9 +3,8 @@
 import { Spinner } from "@/app/components";
 import {
   AlertDialog,
-  AlertDialogDescription,
   Button,
-  Flex,
+  Flex
 } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -20,7 +19,7 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
     try {
       setDeleting(true);
       await axios.delete("/api/issues/" + issueId);
-      router.push("/issues");
+      router.push("/issues/list");
       router.refresh();
     } catch (error) {
       setDeleting(false);
