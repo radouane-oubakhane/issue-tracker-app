048e3c96284e4334bfac780011b7cce957567b5b
radouane <oubakhaneradouane@gmail.com>
2024-01-31 00:17:05 +0100

    Configure Google Provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,17 @@
 import NextAuth from 'next-auth';
+import GoogleProvider from "next-auth/providers/google";
 
 
 
 const handler = NextAuth({
-    providers: []
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+          })
+    ]
 });
 
 
 export { handler as GET, handler as POST };
+
