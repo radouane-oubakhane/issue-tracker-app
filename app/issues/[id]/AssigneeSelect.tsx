c90cbd5274809297d94774cfa734ec858f110407
radouane <oubakhaneradouane@gmail.com>
2024-02-03 01:49:45 +0100

    Add assigned issues to prisma schema

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,17 +1,19 @@
-'use client';
+"use client";
 
-import { User } from '@prisma/client';
-import { Select } from '@radix-ui/themes'
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
+import { User } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import { Skeleton } from "@/app/components";
 
-
-
 const AssigneeSelect = () => {
-  const { data: users, error, isLoading } = useQuery<User[]>({
-    queryKey: ['users'],
-    queryFn: () => axios.get('/api/users').then(res => res.data),
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
     staleTime: 1000 * 60 * 5, // 5 minutes
     retry: 3,
   });
@@ -22,19 +24,19 @@ const AssigneeSelect = () => {
 
   return (
     <Select.Root>
-        <Select.Trigger placeholder='Assign...' />
-        <Select.Content>
-            <Select.Group>
-                <Select.Label>Suggestions</Select.Label>
-                {
-                  users?.map(user => (
-                    <Select.Item key={user.id} value= {user.id}>{user.name}</Select.Item>
-                  ))
-                }
-            </Select.Group>
-        </Select.Content>
+      <Select.Trigger placeholder="Assign..." />
+      <Select.Content>
+        <Select.Group>
+          <Select.Label>Suggestions</Select.Label>
+          {users?.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
+        </Select.Group>
+      </Select.Content>
     </Select.Root>
-  )
-}
+  );
+};
 
-export default AssigneeSelect
+export default AssigneeSelect;
