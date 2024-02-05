ce884a359d0f0afb19f32e7cb01a5b92b5985cd4
radouane <oubakhaneradouane@gmail.com>
2024-02-06 00:10:46 +0100

    Build the LatestIssue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import Image from 'next/image'
+import LatestIssue from './LatestIssue'
 import Pagination from './components/Pagination'
 
-export default function Home({ searchParams }: {searchParams: {page: string}}) {
+export default function Home() {
   return (
-<Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)} />
+    <LatestIssue />
     )
 }
